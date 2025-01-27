a40392590cbc11d91567bb6e8b99e3be5b719478
CAMMCL17 <147645223+CAMMCL17@users.noreply.github.com>
2025-01-27 16:50:54 +0000

    m1lab1

---
+++ m1lab1/m1lab1-mclean.cpp
@@ -0,0 +1,21 @@
+//CSC 134
+// M1Lab1 - The Apple Orchard 
+// Chayse
+//1/27/2025
+// We're going to make the simplest possible 
+//"checkout" machine.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Apples sales porgram
+    //Variables are like mailboxes
+    string name = "Chayse"; //CAM
+    int num_apples = 10;    // int are whole numbers:1, 2, 100000, 42
+    double cost_each =0.25;// twenty five cents, or $0.25.
+
+    cout << "Welcome to the " << Chayse << " apple farm!" << endl;
+
+    cout << endl;
+    return 0; // no errors
