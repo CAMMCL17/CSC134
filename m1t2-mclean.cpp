b8310a4b9c14c265aa094c9ded27f07eaf94f7ab
CAMMCL17 <147645223+CAMMCL17@users.noreply.github.com>
2025-01-27 16:17:48 +0000

    m1t2 started

---
+++ m1t2-mclean.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M1T1 - Hello world 
+// Chayse 
+//1/27/2025
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    cout << "Hello";
+    cout << "Im Chayse Mclean (CAMAlex17 on github)" << endl;
+    cout <<" I watch a lot of call of duty clips."<< endl;
+    // 
+}
+
+
