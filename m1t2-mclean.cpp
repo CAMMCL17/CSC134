120de2905f7e12a5ac4c6ead93c37b227baf1b74
CAMMCL17 <147645223+CAMMCL17@users.noreply.github.com>
2025-01-27 16:21:36 +0000

    m1t2

--- m1t2-mclean.cpp
+++ m1t2-mclean.cpp
@@ -11,7 +11,10 @@ int main()
     cout << "Hello";
     cout << "Im Chayse Mclean (CAMAlex17 on github)" << endl;
     cout <<" I watch a lot of call of duty clips."<< endl;
-    // 
+    // I am 18 
+    // I like playing football and baseball.
+    cout << endl;
+    return 0;
 }
 
 
