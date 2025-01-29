6bf9a153cb7993f01e254eddd1a99ef741799b8c
CAMMCL17 <147645223+CAMMCL17@users.noreply.github.com>
2025-01-29 16:13:37 +0000

    finishing m1t1

--- m1lab1/m1lab1-mclean.cpp
+++ m1lab1/m1lab1-mclean.cpp
@@ -15,8 +15,14 @@ int main() {
     int num_apples = 10;    // int are whole numbers:1, 2, 100000, 42
     double cost_each =0.25;// twenty five cents, or $0.25.
 
-    cout << "Welcome to the " << name << " apple farm!" << endl;
+    cout << "Welcome to the " << name << " apple farm!" << endl
+    cout << "There are " << num_apples << "apples in stock." << endl;
+    cout << "They cost $" << cost_each << " each." << endl;
 
-    cout << endl;
-    return 0; // no errors
+    //Find out the total price 
+    double total_cost = num_apples * cost_each;
+    cout << "The price for all of them is: $" << total_cost << end1;
+
+     cout << endl;
+     return 0; // no errors
 }
