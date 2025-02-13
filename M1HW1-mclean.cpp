ec81a60f7b074c69a6d5271ad32b396bb787c46e
CAMMCL17 <147645223+CAMMCL17@users.noreply.github.com>
2025-02-13 17:39:41 +0000

    M1HW1

---
+++ M1HW1-mclean.cpp
@@ -0,0 +1,13 @@
+// CSC 134
+// M1HW1
+// 2/13/2025
+
+#include <iostream>
+using namespace std;
+
+int main() 
+ {
+    cout << "Friday"
+    cout << "Playing with my money is like playing with my emotions"
+    return 0;
+ }
